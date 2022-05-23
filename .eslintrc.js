8e523e4e90622bc11bd829d4e152c37c2f88a20b
Vladislav <vlad.satonin74@gmail.com>
2022-05-23 19:38:15 +0500

    build!: temporarily comment out some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react'],
+	// extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+	// parserOptions: {
+	// 	ecmaFeatures: {
+	// 		jsx: true,
+	// 	},
+	// 	ecmaVersion: 'latest',
+	// 	sourceType: 'module',
+	// },
+	// plugins: ['react'],
 	rules: {
 		// indent: ['error', 'tab'],
 		// 'linebreak-style': 0,
